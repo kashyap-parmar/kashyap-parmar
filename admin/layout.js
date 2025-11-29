11eeeae80bc15b1e633b2f9fddabd2b142312447
Kashyap Parmar <kashyap209p@gmail.com>
2025-11-29 21:12:23 +0530

    remove admin folder from the root

--- admin/layout.js
+++
@@ -1,6 +0,0 @@
-'use client';
-import { AuthProvider } from '@/contexts/AuthContext';
-
-export default function AdminRootLayout({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
